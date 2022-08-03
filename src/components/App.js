0783b6b6b285facd5a15a573cf33284367a93b64
Vinícius Barros <vicaetano_barros@hotmail.com>
2022-08-03 14:27:48 -0300

    Created the main routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,37 @@
+// import { useState } from "react";
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Habits from "./Habits";
+import LoginPage from "./LoginPage";
+import SignUpPage from "./SignUpPage";
+import GlobalStyle from "./style/GlobalStyle";
+
+import UserContext from "./Context/UserContext";
+import LoginContext from "./Context/LoginContext";
+import Today from "./Today";
+
 function App() {
+  const [user, setUser] = useState(null);
+  const [token, setToken] = useState("");
+  console.log(token, user);
+
   return (
-    <></>
+    <>
+      <GlobalStyle />
+      <UserContext.Provider value={{ user, setUser }}>
+        <LoginContext.Provider value={{ token, setToken }}>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<LoginPage />} />
+              <Route path="/cadastro" element={<SignUpPage />} />
+              <Route path="/habitos" element={<Habits />} />
+              <Route path="/hoje" element={<Today />} />
+            </Routes>
+          </BrowserRouter>
+        </LoginContext.Provider>
+      </UserContext.Provider>
+    </>
   );
 }
 
