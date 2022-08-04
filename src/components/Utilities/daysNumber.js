5463b0fece10b9879c71a0dae0a560741a08e661
Vinícius Barros <vicaetano_barros@hotmail.com>
2022-08-04 15:59:59 -0300

    Added new requests

---
+++ src/components/Utilities/daysNumber.js
@@ -0,0 +1,11 @@
+const daysNumber = [
+    { dia: "domingo", id: 0, sigla: "D" },
+    { dia: "segunda", id: 1, sigla: "S" },
+    { dia: "terca", id: 2, sigla: "T" },
+    { dia: "quarta", id: 3, sigla: "Q" },
+    { dia: "quinta", id: 4, sigla: "Q" },
+    { dia: "sexta", id: 5, sigla: "S" },
+    { dia: "sabado", id: 6, sigla: "S" },
+  ];
+
+export default daysNumber;
--- src/services/TrackIt.js
+++ src/services/TrackIt.js
@@ -3,24 +3,49 @@ import axios from "axios";
 const BASE_URL = "https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit";
 
 function postSignUp(register) {
-    const promise = axios.post(`${BASE_URL}/auth/sign-up`, register);
-    return promise;
+  const promise = axios.post(`${BASE_URL}/auth/sign-up`, register);
+  return promise;
 }
 
 function postSignIn(login) {
-    const promise = axios.post(`${BASE_URL}/auth/login`, login);
-    return promise;
+  const promise = axios.post(`${BASE_URL}/auth/login`, login);
+  return promise;
 }
 
-function postHabit(habit) {
-    const promise = axios.post(`${BASE_URL}/habits`, habit);
-    return promise;
+function postHabit(habit, auth) {
+  const promise = axios.post(`${BASE_URL}/habits`, habit, {
+    headers: {
+      Authorization: `Bearer ${auth}`,
+    },
+  });
+  return promise;
 }
 
 function getHabits(auth) {
-    const promise = axios.get(`${BASE_URL}/habits`, auth);
-    return promise;
+  const promise = axios.get(`${BASE_URL}/habits`, {
+    headers: {
+      Authorization: `Bearer ${auth}`,
+    },
+  });
+  return promise;
 }
 
-export { postSignUp, postSignIn, postHabit, getHabits};
+function deleteHabit(habitId, auth) {
+  const promise = axios.delete(`${BASE_URL}/habits/${habitId}`, {
+    headers: {
+      Authorization: `Bearer ${auth}`,
+    },
+  });
+  return promise;
+}
 
+function getTodayHabits(auth) {
+  const promise = axios.get(`${BASE_URL}/habits/today`, {
+    headers: {
+      Authorization: `Bearer ${auth}`,
+    },
+  });
+  return promise;
+}
+
+export { postSignUp, postSignIn, postHabit, getHabits, deleteHabit, getTodayHabits };
