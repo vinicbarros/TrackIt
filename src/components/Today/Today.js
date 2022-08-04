48ce72af1799cc8d2bb8a7db44e6319ac412ca4a
Vinícius Barros <vicaetano_barros@hotmail.com>
2022-08-04 20:12:14 -0300

    Created the logic with dayjs

--- src/components/Today/Today.js
+++ src/components/Today/Today.js
@@ -1,11 +1,14 @@
 import { useContext, useEffect } from "react";
 import styled from "styled-components";
+import today from "../Utilities/setday";
+
 import { getTodayHabits } from "../../services/TrackIt";
 import LoginContext from "../Context/LoginContext";
 import Navbar from "../Utilities/Navbar";
 import Footer from "../Utilities/Footer";
 
 export default function Today() {
+
   const { token } = useContext(LoginContext);
 
   useEffect(() => {
@@ -17,14 +20,40 @@ export default function Today() {
   return (
     <>
       <Navbar />
-      <TodayContent>vamos tentar novamente</TodayContent>
+      <TodayContent>
+        <h1>{today}</h1>
+        <h3>Nenhum hábito concluído ainda</h3>
+        <HabitsCheckBox>
+            {}
+        </HabitsCheckBox>
+      </TodayContent>
       <Footer />
     </>
   );
 }
 
 const TodayContent = styled.section`
-    margin-top: 70px;
-    min-height: 100vh;
-    background: #f2f2f2;
+  padding-top: 70px;
+  padding-left: 17px;
+  min-height: 100vh;
+  background: #f2f2f2;
+  display: flex;
+  flex-direction: column;
+
+  h1 {
+    margin: 28px 0px 6px 0px;
+    font-weight: 400;   
+    font-size: 23px;
+    color: #126BA5;
+  }
+
+  h3 {
+    color: #BABABA;
+    font-size: 18px;
+    font-weight: 400;
+  }
+`;
+
+const HabitsCheckBox = styled.main`
+
 `;
